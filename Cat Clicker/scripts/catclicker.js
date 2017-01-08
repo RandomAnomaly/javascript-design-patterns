b2124e00920dc6af0d4b767caeef2c3aa404e7e5
jack <jack.gammon@equinox.co.nz>
2017-01-09 11:21:10 +1300

    Completed second cat

--- Cat Clicker/scripts/catclicker.js
+++ Cat Clicker/scripts/catclicker.js
@@ -1,13 +1,33 @@
-var catclicker = (function(){
-    var counter = 0;
 
-    var updateScreen = function(){
-        document.getElementById("counter").innerHTML = counter;
+var displayLogic = (function () {
+
+    this.updateScreen = function (id, value) {
+        document.getElementById(id).innerHTML = value;
     }
 
-    var element = document.getElementById("imgCat");
-    element.addEventListener('click', function(){
-        counter += 1;
-        updateScreen();
+    return this;
+} ());
+
+var catclicker = (function () {
+    var counter1 = 0;
+    var counter2 = 0;
+
+    var cat1name = "Cat One";
+    var cat2name = "Cat Two";
+
+    displayLogic.updateScreen("cat1name", cat1name);
+    displayLogic.updateScreen("cat2name", cat2name);
+
+    var element = document.getElementById("imgCat1");
+    element.addEventListener('click', function () {
+        counter1 += 1;
+        displayLogic.updateScreen("counter1", counter1);
     }, false);
-}())
+
+    element = document.getElementById("imgCat2");
+    element.addEventListener('click', function () {
+        counter2 += 1;
+        displayLogic.updateScreen("counter2", counter2);
+    }, false);
+
+} ());
