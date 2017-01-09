d2e204dd9017ad9f2ddc4b513cd3ead4ba9ef3bf
jack <jack.gammon@equinox.co.nz>
2017-01-10 11:27:10 +1300

    Added a bunch of comments :D

--- Cat Clicker/dev/scripts/catclicker.js
+++ Cat Clicker/dev/scripts/catclicker.js
@@ -37,19 +37,21 @@ var catClicker = (function () {
     /* ================================================================ */
     /* VIEWS                                                            */
     /* ================================================================ */
-
+    // View for the navigation bar
     var navView = {
+        // Creates a set of li buttons from the cat names and attaches it to the DOM
         init: function () {
             var names = octopus.getCatNames();
             for (var i = 0; i < names.length; i += 1) {
                 var button = this.createButton(names[i]);
-
                 document.getElementById("navbar").appendChild(this.createButton(names[i], i));
             }
         },
+        // Sets the given button to inactive
         deselectButton: function (id) {
             document.getElementById("btn" + id).className = "";
         },
+        // Sets the given button to active
         selectButton: function (id) {
             document.getElementById("btn" + id).className = "active";
         },
@@ -69,29 +71,23 @@ var catClicker = (function () {
         }
     };
 
+    // View for the main display area
     var catAreaView = {
+        // Initialise the view - all this really does is attach the event listener to the image
         init: function () {
             document.getElementById("catPhoto").addEventListener('click', function () {
                 octopus.incrementClicker();
             });
         },
+        // Render the currently selected cat, updates the name, clicker and photo from the json
         render: function () {
             var activeCat = octopus.getActiveCat();
             document.getElementById("catName").innerText = activeCat.name;
-            //document.getElementById("catPhoto").removeChild(document.getElementById("catPhoto").firstChild);
-            //document.getElementById("catPhoto").appendChild(this.createPhoto(activeCat.imageSrc));
             var photo = document.getElementById("catPhoto");
             photo.src = activeCat.imageSrc;
-
-
-
             this.updateClicker(activeCat.counter);
         },
-        createPhoto: function (imageSrc) {
-            var photo = document.createElement("img");
-            photo.src = imageSrc;
-            return photo;
-        },
+        // Sets the clicker on screen to the given value
         updateClicker: function (value) {
             document.getElementById("catTicker").innerText = value;
         }
@@ -101,14 +97,18 @@ var catClicker = (function () {
     /* CONTROLLER                                                       */
     /* ================================================================ */
     var octopus = {
+        // Initiallise the nav view, cat area view and set the selected cat to zero
+        // in the model
         init: function () {
             navView.init();
             catAreaView.init();
             this.selectCat(0);
         },
+        // Returns an object representing the currently selected cat
         getActiveCat: function () {
             return model.data[model.selectedCat];
         },
+        // Returns an array of the names of the cats in the cats collection
         getCatNames: function () {
             var names = [];
             for (var i = 0; i < model.data.length; i += 1) {
@@ -116,6 +116,7 @@ var catClicker = (function () {
             }
             return names;
         },
+        // Sets the active cat and deactivates / activates the corresponding buttons
         selectCat: function (id) {
             navView.deselectButton(model.selectedCat);
             model.selectedCat = id;
@@ -123,11 +124,12 @@ var catClicker = (function () {
             var cat = model.data[id];
             catAreaView.render();
         },
+        // Increments the clicker and updates the view to relfect this
         incrementClicker: function () {
             model.data[model.selectedCat].counter += 1;
             catAreaView.updateClicker(model.data[model.selectedCat].counter);
         }
     };
-
+    // kick everything off (:
     octopus.init();
 } ());
