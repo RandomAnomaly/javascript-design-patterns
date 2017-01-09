179b06a6fea30256f8126166a4f6a5c4dda1b1d2
jack <jack.gammon@equinox.co.nz>
2017-01-09 13:19:14 +1300

    Refactored to allow easy adding of additional cats

---
+++ Cat Clicker/scripts/cat_list.js
@@ -0,0 +1,27 @@
+var cats = [
+    {
+        "name": "Scrunchie",
+        "imageSrc": "bin/cat.jpg",
+        "counter": 0
+    },
+    {
+        "name": "Schrodinger",
+        "imageSrc": "bin/cat2.jpg",
+        "counter": 0
+    },
+    {
+        "name": "Snuggles",
+        "imageSrc": "bin/cat3.jpg",
+        "counter": 0
+    },
+    {
+        "name": "Wobbles",
+        "imageSrc": "bin/cat4.jpg",
+        "counter": 0
+    },
+    {
+        "name": "Catrhito",
+        "imageSrc": "bin/cat5.jpg",
+        "counter": 0
+    }
+];
--- Cat Clicker/scripts/catclicker.js
+++ Cat Clicker/scripts/catclicker.js
@@ -1,33 +1,83 @@
+var catclicker = (function () {
 
-var displayLogic = (function () {
+    // todo optim
+    var pageFactory = (function () {
+        for (var i = 0; i < cats.length; i += 1) {
+            // build the navbar
+            var listItem = document.createElement("li");
+            listItem.id = "catButton" + i;
+            listItem.role = "presentation";
+            listItem.className = "NavButton";
 
-    this.updateScreen = function (id, value) {
-        document.getElementById(id).innerHTML = value;
+            listItem.addEventListener('click', (function (numCopy) {
+                return function () {
+                    activateTab(numCopy);
+                };
+            })(i));
+
+            var innerLink = document.createElement("a");
+            innerLink.href = "#";
+            innerLink.innerText = cats[i].name;
+            listItem.appendChild(innerLink);
+            document.getElementById("navBar").appendChild(listItem);
+
+            var displayArea = document.getElementById("display");
+            var innerDiv = document.createElement("div");
+            innerDiv.id = "catDisplay" + i;
+            innerDiv.className = "CatDisplay"
+            innerDiv.hidden = true;
+
+            var catName = document.createElement("div");
+            catName.innerText = cats[i].name;
+            innerDiv.appendChild(catName);
+
+            var catPhoto = document.createElement("img");
+            catPhoto.src = cats[i].imageSrc;
+
+            catPhoto.addEventListener('click', (function(numCopy){
+                return function(){
+                    incrementCounter(numCopy);
+                };
+            })(i));
+
+            innerDiv.appendChild(catPhoto);
+
+            var catCounter = document.createElement("div");
+            catCounter.innerText = cats[i].counter;
+            catCounter.id = "catCounter" + i;
+            innerDiv.appendChild(catCounter);
+
+            displayArea.appendChild(innerDiv);
+        }
+
+
+        //activate a tab
+    } ());
+
+
+    var incrementCounter = function(toIncrement){
+        cats[toIncrement].counter += 1;
+        document.getElementById("catCounter" +  toIncrement).innerText = cats[toIncrement].counter;
     }
 
-    return this;
-} ());
+    var activateTab = function (toActivate) {
+        // deactivate all tabs
+        var navs = document.getElementsByClassName("NavButton");
+        for (var i = 0; i < navs.length; i += 1) {
+            navs[i].className = "NavButton";
+        }
 
-var catclicker = (function () {
-    var counter1 = 0;
-    var counter2 = 0;
+        // hide all displays
+        var displays = document.getElementsByClassName("CatDisplay");
+        for (var i = 0; i < displays.length; i += 1) {
+            displays[i].hidden = true;
+        }
 
-    var cat1name = "Cat One";
-    var cat2name = "Cat Two";
+        // activate the new one
+        document.getElementById("catButton" + toActivate).className = "active NavButton";
 
-    displayLogic.updateScreen("cat1name", cat1name);
-    displayLogic.updateScreen("cat2name", cat2name);
-
-    var element = document.getElementById("imgCat1");
-    element.addEventListener('click', function () {
-        counter1 += 1;
-        displayLogic.updateScreen("counter1", counter1);
-    }, false);
-
-    element = document.getElementById("imgCat2");
-    element.addEventListener('click', function () {
-        counter2 += 1;
-        displayLogic.updateScreen("counter2", counter2);
-    }, false);
+        // show the relevant display
+        document.getElementById("catDisplay" + toActivate).hidden = false;
+    }
 
 } ());
