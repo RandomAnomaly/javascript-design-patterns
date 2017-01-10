e12ce0339963e7c29795d921ec84f2fe49fc336b
jack <jack.gammon@equinox.co.nz>
2017-01-10 16:55:42 +1300

    Completed cats list - knockout

--- ud989-cat-clicker-ko-starter/js/app.js
+++ ud989-cat-clicker-ko-starter/js/app.js
@@ -1,10 +1,47 @@
-var Cat = function () {
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('Tabby');
-    this.imgSrc = ko.observable('img/22252709_010df3379e_z.jpg');
-    this.imgAttribution = ko.observable('');
+var initialCats = [{
+    "name": "Scrunchie",
+    "imgSrc": "img/1413379559_412a540d29_z.jpg",
+    "counter": 0,
+    "imgAttribution": "",
+    "nicknames": []
+},
+{
+    "name": "Schrodinger",
+    "imgSrc": "img/22252709_010df3379e_z.jpg",
+    "counter": 0,
+    "imgAttribution": "",
+    "nicknames": []
+},
+{
+    "name": "Snuggles",
+    "imgSrc": "img/4154543904_6e2428c421_z.jpg",
+    "counter": 0,
+    "imgAttribution": "",
+    "nicknames": []
+},
+{
+    "name": "Wobbles",
+    "imgSrc": "img/434164568_fea0ad4013_z.jpg",
+    "counter": 0,
+    "imgAttribution": "",
+    "nicknames": []
+},
+{
+    "name": "Burrcato",
+    "imgSrc": "img/9648464288_2516b35537_z.jpg",
+    "counter": 0,
+    "imgAttribution": "",
+    "nicknames": []
+}
+];
+
+var Cat = function (data) {
+    this.clickCount = ko.observable(data.counter);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+    this.imgAttribution = ko.observable(data.imgAttribution);
     this.levels = ['Newborn', 'Toddler', 'Young Adult', 'Middle-Aged', 'Elderly', 'Very Old', 'Ancient', 'Eldritchian']
-    this.nicknames = ko.observableArray(['a', 'b', 'c', 'd', 'e']);
+    this.nicknames = ko.observableArray(data.nicknames);
 
     this.level = ko.computed(function () {
         var index = this.clickCount() / 10;
@@ -15,14 +52,26 @@ var Cat = function () {
 
 
 var ViewModel = function () {
+    var self = this; // for storing the ViewModel context
 
-    this.currentCat = ko.observable(new Cat());
+    this.catList = ko.observableArray([]);
+
+    initialCats.forEach(function (catItem) {
+        self.catList.push(new Cat(catItem));
+    });
+
+
+
+    this.currentCat = ko.observable(this.catList()[0]);
 
     this.incrementCounter = function () {
-        this.clickCount(this.clickCount() + 1);
+        self.currentCat().clickCount(self.currentCat().clickCount() + 1);
     };
 
-
+    this.changeCat = function (cat) {
+        console.log(cat);
+        self.currentCat(cat);
+    }
 }
 
 ko.applyBindings(new ViewModel());
